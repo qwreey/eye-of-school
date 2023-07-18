e73d23264ca2c1e0274037948ee3e4c9599d6bf6
qwreey75 <qwreey75@gmail.com>
2023-07-19 08:55:02 +0900

    Add base mongodb loader

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,29 @@ import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
 import fastify from "fastify"
 import { Type } from '@sinclair/typebox'
 import { configDotenv } from "dotenv"
+import mongodb from "mongodb"
 
-configDotenv()
+configDotenv() // .env 불러오기
 
 const instance = fastify({ logger: true }).withTypeProvider<TypeBoxTypeProvider>()
-export type instance = typeof instance
 
+// DB 등록
+const mongoInstance = await new mongodb.MongoClient(process.env.MONGOURL || "mongodb://localhost:19722").connect()
+const db = mongoInstance.db(process.env.MONGODB || "eos")
+instance.decorate("db",db)
+
+// 라우터 등록
+import
+instance.register()
+
+// 시작 & 타입
 instance.listen({
     port: parseInt(process.env.PORT || '3000'),
     host: process.env.HOST || 'localhost',
 })
+declare module 'fastify' {
+    interface FastifyInstance {
+        db: mongodb.Db
+    }
+}
+export type instance = typeof instance
